ebcecfc205172088343661f249828902e1451e1a
Gary <66178774+gen8787@users.noreply.github.com>
2021-03-18 15:09:18 -0700

    next / prev, html format

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("post/<int:pk>", BlogDetailView.as_view(), name="one_post"),
     path("post/<int:post_id>/like", like_post, name="like_post"),
     path("post/<int:post_id>/dislike", dislike_post, name="dislike_post"),
+    path("post/<int:post_id>/previous", previous_post, name="previous_post"),
+    path("post/<int:post_id>/next", next_post, name="next_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ class BlogListView(ListView):
     context_object_name = "all_posts_list"
 
 
-# ---- O N E   B L O G S
+# ---- O N E   B L O G
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'one_post.html'
@@ -31,3 +31,23 @@ def dislike_post(request, post_id):
     disliked_post.dislikes += 1
     disliked_post.save()
     return redirect('/')
+
+
+# ---- P R E V I O U S   P O S T
+def previous_post(request, post_id):
+    if (post_id == 1):
+        return redirect('one_post', pk=1)
+
+    else:
+        return redirect('one_post', pk=post_id - 1)
+
+
+# ---- N E X T   P O S T
+def next_post(request, post_id):
+    last_post_id = Post.objects.last().id
+
+    if (post_id == last_post_id):
+        return redirect('one_post', pk=post_id)
+
+    else:
+        return redirect('one_post', pk=post_id + 1)
